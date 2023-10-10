18fd4737455052871a142b54fd701ec1b1359c43
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-10 16:52:47 +0530

    Initial Version

--- src/S2/a3-completion-cs/CompletionDemo/CompletionDemo/Program.cs
+++ src/S2/a3-completion-cs/CompletionDemo/CompletionDemo/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Note: the Azure OpenAI client library for .NET is in preview.
+// Install the .NET library via NuGet: dotnet add package Azure.AI.OpenAI --version 1.0.0-beta.8
+
+using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Configuration;
+
+var _configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets<Program>()
+    .Build();
