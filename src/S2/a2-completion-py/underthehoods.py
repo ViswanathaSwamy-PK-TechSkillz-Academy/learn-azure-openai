4f1cf0f2e96d8d90d21d1ea1e3c1b0319f3341b1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-10 17:49:16 +0530

    Go Lang

--- src/S2/a2-completion-py/underthehoods.py
+++ src/S2/a2-completion-py/underthehoods.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 openai.api_type = "azure"
-openai.api_base = "https://azure-openai-dev-001.openai.azure.com/"
+openai.api_base = "https://azure-YOUR-dev-001.openai.azure.com/"
 openai.api_version = "2022-12-01"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
--- src/S2/a4-completion-go/A1CompletionDemo/main.go
+++ src/S2/a4-completion-go/A1CompletionDemo/main.go
@@ -13,7 +13,7 @@ func main() {
 	azureOpenAIKey := os.Getenv("OPENAI_API_KEY")
 	modelDeploymentID := "text-davinci-003-dev-0109"
 
-	azureOpenAIEndpoint := os.Getenv("AZURE_OPENAI_ENDPOINT")
+	azureOpenAIEndpoint := "https://azure-YOUR-dev-001.openai.azure.com/" // os.Getenv("AZURE_OPENAI_ENDPOINT")
 
 	if azureOpenAIKey == "" || modelDeploymentID == "" || azureOpenAIEndpoint == "" {
 		fmt.Fprintf(os.Stderr, "Skipping example, environment variables missing\n")
@@ -24,22 +24,26 @@ func main() {
 
 	if err != nil {
 		// TODO: handle error
+		print(err.Error())
 	}
 
 	client, err := azopenai.NewClientWithKeyCredential(azureOpenAIEndpoint, keyCredential, nil)
 
 	if err != nil {
 		// TODO: handle error
+		print(err.Error())
 	}
 
 	resp, err := client.GetCompletions(context.TODO(), azopenai.CompletionsOptions{
 		Prompt:      []string{"What is Azure OpenAI, in 20 words or less"},
 		MaxTokens:   to.Ptr(int32(2048)),
 		Temperature: to.Ptr(float32(0.0)),
+		Deployment:  modelDeploymentID,
 	}, nil)
 
 	if err != nil {
 		// TODO: handle error
+		print(err.Error())
 	}
 
 	for _, choice := range resp.Choices {
