37cdd4132f953107127c63f7aa52265e158207fb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-10 17:12:22 +0530

    Changes

--- src/S2/a3-completion-cs/CompletionDemo/CompletionDemo/Program.cs
+++ src/S2/a3-completion-cs/CompletionDemo/CompletionDemo/Program.cs
@@ -3,6 +3,7 @@
 
 using Azure;
 using Azure.AI.OpenAI;
+using HeaderFooter;
 using Microsoft.Extensions.Configuration;
 
 var _configuration = new ConfigurationBuilder()
@@ -10,3 +11,42 @@ var _configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddUserSecrets<Program>()
     .Build();
+
+Header _header = new();
+Footer _footer = new();
+
+OpenAIClient client = new(new Uri(_configuration["CompletionConfiguration:OPEN_API_EndPoint"]!),
+                            new AzureKeyCredential(Env.GetEnvironmentVariable("OPENAI_API_KEY")!));
+
+const string prompt = "What are the top 10 countries with highest populations are along with their population count and capital city : \n";
+
+Response<Completions> completionsResponse = await client.GetCompletionsAsync(
+    deploymentOrModelName: _configuration["CompletionConfiguration:ModelDeploymentName"]!,
+    new CompletionsOptions()
+    {
+        Prompts = { prompt },
+        Temperature = (float)1,
+        MaxTokens = 120,
+        NucleusSamplingFactor = (float)0.5,
+        FrequencyPenalty = (float)0,
+        PresencePenalty = (float)0,
+        GenerationSampleCount = 1,
+        Echo = true
+    });
+
+_header.DisplayHeader('=', "Azure Open AI Completion Demo");
+
+ForegroundColor = ConsoleColor.DarkCyan;
+
+int index = 0;
+foreach (string? text in completionsResponse.Value?.Choices?.Select(c => c.Text)?.ToArray()!)
+{
+    WriteLine($"{++index}. {text}");
+}
+
+_footer.DisplayFooter('-');
+
+ResetColor();
+
+WriteLine("\nPress any key ...");
+ReadKey();
