3b37ffbf275cba4731c856f8a7d43038968fdffc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-23 16:28:15 +0530

    Changes

--- src/Cgpt.Azoai/Services.Azoai/Common/AzureOpenAiRoutes.cs
+++ src/Cgpt.Azoai/Services.Azoai/Common/AzureOpenAiRoutes.cs
@@ -4,7 +4,7 @@ public static partial class Constants
 {
     public static class AzureOpenAiRoutes
     {
-        public static string Prefix { get; } = "/api/countries";
+        public static string Prefix { get; } = "/api/getcompletion";
 
         public static string Root => "/";
     }
---
+++ src/Cgpt.Azoai/Services.Azoai/Endpoints/AzureOpenAIEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Services.Azoai.Endpoints;
+
+public static class AzureOpenAIEndpoints
+{
+
+    public static void MapCountriesEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup(AzureOpenAiRoutes.Prefix).WithTags("AzureOpenAIEndpoints.API");
+
+        _ = group.MapGet(AzureOpenAiRoutes.Root, async ([FromServices] ICountriesBusiness countriesBusiness) =>
+        {
+            return Results.Ok(await countriesBusiness.GetAllCountries());
+
+        })
+          .AllowAnonymous()
+          .WithName("GetAllCountries")
+          .Produces<IReadOnlyCollection<CountryInfo>>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status500InternalServerError)
+          .WithOpenApi();
+    }
+
+}
