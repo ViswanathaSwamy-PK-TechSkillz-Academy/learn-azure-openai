20ed8fa60d1aee19efdca530868bcec0ba5a58b2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-23 17:36:31 +0530

    Verified Chat GPT Clone

---
+++ src/Cgpt.Azoai/Services.Azoai/wwwroot/scripts/index.js
@@ -0,0 +1,31 @@
+function generateResponseFromAOAI() {
+    const userInput = document.getElementById("userInput").value;
+    const currentResponse = document.getElementById("current-response");
+    const chatMessagesHistory = document.getElementById("chat-messages-history");
+
+    // Append the user's input to the chat
+    chatMessagesHistory.innerHTML += `<li class="list-group-useritem"><b>You:</b> ${userInput}</li>`;
+
+    // Make an API request to your Flask API
+    fetch("/api/getcompletion?userInput=" + userInput)
+        .then(response => response.text())
+        .then(data => {
+            data = processResponse(data);
+
+            // Append the AI's response to the chat Messages History
+            chatMessagesHistory.innerHTML += `<li class="list-group-azoaiitem"><b>Azure Open AI:</b> ${data}</li>`;
+            console.log("Success:", data);
+            currentResponse.innerText = data;
+        })
+        .catch(error => {
+            console.error("An error occurred:", error);
+            currentResponse.innerText = "An error occurred while processing the request.";
+            chatMessagesHistory.innerHTML += `<li class="list-group-azoaiitem"><b>Azure Open AI:</b> An error occurred while processing the request.</li>`;
+        });
+}
+
+// Process the API response and remove unwanted <br> tags
+function processResponse(data) {
+    data = data.replace(/^<br>/g, '').trim();
+    return data;
+}
