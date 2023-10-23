479f28c486380593a9061dcc5a1105cf74e84d29
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-23 16:22:05 +0530

    Changes

---
+++ src/Cgpt.Azoai/Services.Azoai/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Services.Azoai.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        _ = services.AddEndpointsApiExplorer();
+        _ = services.AddSwaggerGen();
+
+        return services;
+    }
+
+}
--- src/Cgpt.Azoai/Services.Azoai/Program.cs
+++ src/Cgpt.Azoai/Services.Azoai/Program.cs
@@ -1,31 +1,16 @@
-using Services.Azoai.Endpoints;
+using Services.Azoai.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ConfigureApplicationServices();
 
 // Configure static file serving
 builder.WebHost.UseWebRoot("wwwroot");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.MapWelcomeEndpoints();
+app.ConfigureHttpRequestPipeline();
 
 app.Run();
 
