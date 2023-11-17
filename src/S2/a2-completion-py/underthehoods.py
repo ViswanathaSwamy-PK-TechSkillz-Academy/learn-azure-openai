e40ec1d3bc6b10a37fa986969bab998d8c9ead06
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-17 16:46:23 +0530

    Code Refactoring

--- src/S2/a2-completion-py/underthehoods.py
+++ src/S2/a2-completion-py/underthehoods.py
@@ -1,27 +1,29 @@
 import os
 import openai
+from dotenv import dotenv_values
+
+# Load config values
+config_details = dotenv_values(".env")
 
 openai.api_type = "azure"
-openai.api_base = "https://azure-YOUR-dev-001.openai.azure.com/"
-openai.api_version = "2022-12-01"
+openai.api_base = config_details['OPENAI_API_BASE']
+openai.api_version = config_details['OPENAI_API_VERSION']
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 user_prompt = "Write a small tweet for twitter; on my Dosa Center. 1001 varities, home made spices, butter, and lots of love. I need 5 tweets."
 
 response = openai.Completion.create(
-  engine="gpt-35-turbo-dev-001",
-  prompt=user_prompt,
-  temperature=1,
-  max_tokens=150,
-  top_p=0.5,
-  frequency_penalty=0,
-  presence_penalty=0,
-  stop=None)
-
-# print('Created: ', response['created'], 'Choices: ', response['choices'])
+    engine=config_details['OPENAI_ENGINE'],
+    prompt=user_prompt,
+    temperature=1,
+    max_tokens=150,
+    top_p=0.5,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None)
 
 # print(response)
-
+# print('Created: ', response['created'], 'Choices: ', response['choices'])
 print(response.choices[0].text)
 # print(response.choices[1].text)
 # print(response.choices[2].text)
@@ -29,4 +31,3 @@ print(response.choices[0].text)
 # print(response.choices[4].text)
 
 # print(type(response))
-
