31a679750866c8cd2c21e9a4e49fa857292934ee
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-27 15:26:54 +0530

    Update index.js

--- learnaiprojectsints/basics/src/index.js
+++ learnaiprojectsints/basics/src/index.js
@@ -5,18 +5,24 @@ const azureApiKey = process.env["OPENAI_API_KEY"];
 
 const credential = new AzureKeyCredential(azureApiKey);
 
+const messages = [
+    { role: "system", content: "You are a helpful assistant." },
+    { role: "user", content: "Does Azure OpenAI support customer managed keys?" },
+    { role: "assistant", content: "Yes, customer managed keys are supported by Azure OpenAI" },
+    { role: "user", content: "Do other Azure AI services support this too" },
+];
+
 async function main() {
-    console.log("== Get completions Sample ==");
+    await showCompletionsDemo();
 
-    const client = new OpenAIClient(endpoint, credential);
+    console.log("== Chat Completions Sample ==");
 
-    const deploymentId = "text-davinci-003-dev-0109";
-    const prompt = "Once upon a time, ";
-
-    const result = await client.getCompletions(deploymentId, prompt);
+    const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+    const deploymentId = "gpt-35-turbo";
+    const result = await client.getChatCompletions(deploymentId, messages);
 
     for (const choice of result.choices) {
-        console.log(choice.text);
+        console.log(choice.message);
     }
 }
 
@@ -25,3 +31,18 @@ main().catch((err) => {
 });
 
 export default { main };
+
+async function showCompletionsDemo() {
+    console.log("== Get completions Sample ==");
+
+    const client = new OpenAIClient(endpoint, credential);
+
+    const deploymentId = "text-davinci-003-dev-0109";
+    const prompt = "What is an apple?";
+
+    const result = await client.getCompletions(deploymentId, prompt);
+
+    for (const choice of result.choices) {
+        console.log(choice.text);
+    }
+}
